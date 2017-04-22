b226d16984ead2573976d4749f1ea0b16f3dfe62
Spike <spike@drba.org>
2017-04-22 16:06:41 -0700

    - change default pxe booting mode to textmode instead of vga. this makes   ncurses based install with qemu possible and works better on server   too with low gfx capabilities - migrated memtest to use the memtest iso from passmark which has a few   more debugging options - tuned apt mirror variables - fixed but with security mirror being installed with the wrong path

--- roles/netinstall/files/preseed-postinst.sh
+++ roles/netinstall/files/preseed-postinst.sh
@@ -7,8 +7,9 @@ unset DEBIAN_HAS_FRONTEND
 unset DEBIAN_FRONTEND
 
 TASKSEL=$1
+PRESEED_LOCK="/var/tmp/preseed_lock"
 
-apt-get install -y openssh-server python-minimal >> /var/log/preseed.log
+apt-get install -y python-minimal
 
 # install admin ssh key for unattended operations
 mkdir /home/administrator/.ssh
@@ -17,13 +18,17 @@ echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYi3OOvRQAJnIZk+nTH15aJs0AOVSLaSN6TH
 chown -R administrator: /home/administrator/.ssh
 
 # get ssh started so I can run ansible
-mkdir /var/run/sshd
+mkdir -p -m0755 /var/run/sshd
 /usr/sbin/sshd -4 -p22
 
-if [ $TASKSEL != "none" ]
+if [ "$TASKSEL" != "none" ]
 then
   tasksel install $TASKSEL
 fi
-# to allow ansible to work
-#echo "administrator ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/administrator
-#chmod 0440 /etc/sudoers.d/administrator
+
+# the system won't reboot until the lock is cleared
+touch $PRESEED_LOCK
+while [ -f $PRESEED_LOCK ]
+do
+  sleep 60
+done
